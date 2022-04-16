9cb1cd337e2c164a7a923cc0fe72272af4b34d0e
Piyush Aggarwal <piyushaggarwal801@gmail.com>
2022-04-16 16:20:08 +0530

    added mongo database

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const Note = require('./models/noteModel')
 
 app.use(cors())
 
@@ -9,59 +11,36 @@ app.use(express.static('build'))
 // json-parser
 app.use(express.json())
 
-let notes = [
-  {
-    id: 1,
-    content: 'HTML is easy',
-    date: '2022-05-30T17:30:31.098Z',
-    important: true,
-  },
-  {
-    id: 2,
-    content: 'Browser can execute only Javascript',
-    date: '2022-05-30T18:39:34.091Z',
-    important: false,
-  },
-  {
-    id: 3,
-    content: 'GET and POST are the most important methods of HTTP protocol',
-    date: '2022-05-30T19:20:14.298Z',
-    important: true,
-  },
-]
-
-app.get('/', (req, res) => {
-  res.send('<h1>Notes App backend</h1>')
+app.get('/', (request, response) => {
+  response.send('<h1>Notes App backend</h1>')
 })
 
-app.get('/api/notes', (req, res) => {
-  res.json(notes)
-})
-
-app.get('/api/notes/:id', (req, res) => {
-  const id = +req.params.id
-  const note = notes.find((note) => {
-    return note.id === id
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then((notes) => {
+    response.json(notes)
   })
-
-  if (note) {
-    res.json(note)
-  } else {
-    res.status(404).end()
-  }
 })
 
-app.delete('/api/notes/:id', (req, res) => {
-  const id = +req.params.id
-  notes = notes.filter((note) => note.id !== id)
-
-  res.status(204).end()
+app.get('/api/notes/:id', (request, response) => {
+  Note.findById(request.params.id)
+    .then((note) => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch((error) => {
+      console.log(error)
+      response.status(400).send({ error: 'malformatted id' })
+    })
 })
 
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0
-  return maxId + 1
-}
+// app.delete('/api/notes/:id', (request, response) => {
+//   Note.findById(request.params.id).then((note) => {
+//     response.json(note)
+//   })
+// })
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
@@ -72,15 +51,15 @@ app.post('/api/notes', (request, response) => {
     })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note)
-  response.json(note)
+  note.save().then((savedNote) => {
+    response.json(savedNote)
+  })
 })
 
 const PORT = process.env.PORT || 3001
---
+++ models/noteModel.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB', error.message)
+  })
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  data: Date,
+  important: Boolean,
+})
+
+// formatting the returned object by database
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    // Even though the _id property of Mongoose objects looks like a string, it is in fact an object.
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
+
+module.exports = mongoose.model('Note', noteSchema)
